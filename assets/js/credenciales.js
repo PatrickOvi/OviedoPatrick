0cbaffdfd58724c06063d88a86f35cea65b1a7a5
PatrickOvi <dinokillersyt@gmail.com>
2024-06-11 11:50:39 -0400

    fin parte1
    punto 1: no lo pude hacer simplemente porque no logre acordarme como hacerlo.
    punto 2: realice el formulario con todo lo que se solicito, pero no pude realizar las validaciones por la misma razon que el punto 1.

---
+++ assets/js/credenciales.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDtR5OHe7EewZyA6ZrDf_cStt59Mj4L0qA",
+    authDomain: "eva3patrick.firebaseapp.com",
+    projectId: "eva3patrick",
+    storageBucket: "eva3patrick.appspot.com",
+    messagingSenderId: "1047654412330",
+    appId: "1:1047654412330:web:6e442ed52c062659845353"
+  };
